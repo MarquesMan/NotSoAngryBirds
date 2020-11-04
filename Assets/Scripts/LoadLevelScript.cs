e6695f2b883574bc75a09523ce133f9ff003ef9a
MarquesMan <marquesnavarezi@hotmail.com>
2020-11-04 07:55:45 -0400

    Fim dia 4 - Level Select

---
+++ Assets/Scripts/LoadLevelScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadLevelByGameObjectName()
+    {
+        SceneManager.LoadScene(gameObject.name);
+    }
+
+    public void LoadLevelByLevelName(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
